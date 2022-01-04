eb1b0fcf24aa74d9442d82cb5be268f4d08c628f
Jonathan Gerbaud <jonathan.gerbaud@gmail.com>
2022-01-04 15:52:05 +0000

    [UI] Sidebar is now fully working on all state changes

--- libs/ui/src/lib/sidebar/Sidebar.tsx
+++ libs/ui/src/lib/sidebar/Sidebar.tsx
@@ -7,16 +7,16 @@ import usePrevious from '../hooks/usePrevious';
 
 interface SidebarProps extends SoperioComponent, ParentComponent
 {
-    side?: "start" | "end" | "top" | "bottom",
-    closeOnMaskClick?: boolean,
-    closeOnEsc?: boolean,
-    show: boolean,
-    size?: string,
-    onClose?: () => void
+  side?: "start" | "end" | "top" | "bottom",
+  closeOnMaskClick?: boolean,
+  closeOnEsc?: boolean,
+  show: boolean,
+  size?: string,
+  onClose?: () => void;
 }
 
-const DEFAULT_DURATION = "500"
-const DEFAULT_EASING = "in"
+const DEFAULT_DURATION = "500";
+const DEFAULT_EASING = "in";
 
 /**
  *
@@ -24,71 +24,70 @@ const DEFAULT_EASING = "in"
  */
 export function Sidebar({ side = "start", onClose, show = false, children, ...props }: SidebarProps)
 {
-    const [internalShow, setInternalShow] = React.useState(false)
-    const previousSide = usePrevious(side)
+  const [internalShow, setInternalShow] = React.useState(false);
+  const previousSide = usePrevious(side);
 
-    const firstRender = useFirstRender();
-    const direction = useDirection()
+  const firstRender = useFirstRender();
+  const direction = useDirection();
+  const previousDirection = usePrevious(direction);
 
-    const isX = side === "start" || side === "end"
-    const width = isX ? "1/5" : "full";
-    const height = isX ? "full" : "1/5";
+  const isX = side === "start" || side === "end";
+  const width = isX ? "1/5" : "full";
+  const height = isX ? "full" : "1/5";
 
-    const startDirection = (side === "start" && direction) || (side === "end" && !direction)
-    console.log("sidebar start direction", startDirection)
-    console.log("sidebar isX", isX)
+  const startDirection = (side === "start" && direction) || (side === "end" && !direction);
 
-    const translateX = isX ? (show ? "0" : startDirection ? "-full" : "full") : "0"
-    const translateY = !isX ? (show ? "0" : (side === "top" ? "-full" : "full")) : "0"
-    const initTranslateX = isX ? (startDirection ? "-full" : "full") : "0"
-    const initTranslateY = !isX ? (side === "top" ? "-full" : "full") : "0"
-    const justify = startDirection || side === "top" ? "start" : "end"
+  const translateX = isX ? (show ? "0" : startDirection ? "-full" : "full") : "0";
+  const translateY = !isX ? (show ? "0" : (side === "top" ? "-full" : "full")) : "0";
+  const initTranslateX = isX ? (startDirection ? "-full" : "full") : "0";
+  const initTranslateY = !isX ? (side === "top" ? "-full" : "full") : "0";
+  const justify = side === "start" || side === "top" ? "start" : "end";
 
-    React.useEffect(() =>
-    {
-        if (show !== internalShow)
-            setInternalShow(show);
-    }, [show, internalShow, setInternalShow])
+  React.useEffect(() =>
+  {
+    if (show !== internalShow)
+      setInternalShow(show);
+  }, [show, internalShow, setInternalShow]);
 
-    // TODO This is not working when changing side
-    // See https://github.com/DouyinFE/semi-design/blob/main/packages/semi-animation-react/src/Transition.tsx
-    // https://github.com/DouyinFE/semi-design/blob/main/packages/semi-ui/sideSheet/SideSheetTransition.tsx
-    // For a Transition component
+  // Another side bar implementation
+  // See https://github.com/DouyinFE/semi-design/blob/main/packages/semi-animation-react/src/Transition.tsx
+  // https://github.com/DouyinFE/semi-design/blob/main/packages/semi-ui/sideSheet/SideSheetTransition.tsx
+  // For a Transition component
 
-    return (
-        <div
-            z="1000"
-            w="screen"
-            h="screen"
-            position="absolute"
-            overflow="hidden"
-            inset="0"
-            bgColor="black"
-            transition={firstRender ? "none" : "colors"}
-            easing={props.easing || DEFAULT_EASING}
-            duration={props.duration || DEFAULT_DURATION}
-            onClick={onClose}
-            bgOpacity={internalShow ? 50 : 0}
-            dflex
-            flexDirection={isX ? "row" : "column"}
-            justifyContent={justify}
-            alignContent={justify}
-            pointerEvents={show ? "auto" : "none"}
-            {...props}
-        >
-            <div
-                w={width}
-                h={height}
-                bgColor={props.bgColor || "white"} // TODO theme() function to get theme.bg-color-1
-                transition={firstRender || side === previousSide ? "transform" : "none"}
-                transform
-                easing={props.easing || DEFAULT_EASING}
-                duration={props.duration || DEFAULT_DURATION}
-                translateX={side === previousSide ? translateX : initTranslateX}
-                translateY={side === previousSide ? translateY : initTranslateY}
-            >
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    <div
+      z="1000"
+      w="screen"
+      h="screen"
+      position="absolute"
+      overflow="hidden"
+      inset="0"
+      bgColor="black"
+      transition={firstRender ? "none" : "colors"}
+      easing={props.easing || DEFAULT_EASING}
+      duration={props.duration || DEFAULT_DURATION}
+      onClick={onClose}
+      bgOpacity={internalShow ? 50 : 0}
+      dflex
+      flexDirection={isX ? "row" : "column"}
+      justifyContent={justify}
+      alignContent={justify}
+      pointerEvents={show ? "auto" : "none"}
+      {...props}
+    >
+      <div
+        w={width}
+        h={height}
+        bgColor={props.bgColor || "white"} // TODO theme() function to get theme.bg-color-1
+        transition={firstRender || (side === previousSide && direction === previousDirection) ? "transform" : "none"}
+        transform
+        easing={props.easing || DEFAULT_EASING}
+        duration={props.duration || DEFAULT_DURATION}
+        translateX={side === previousSide && previousDirection === direction ? translateX : initTranslateX}
+        translateY={side === previousSide && previousDirection === direction ? translateY : initTranslateY}
+      >
+        {children}
+      </div>
+    </div>
+  );
 }
