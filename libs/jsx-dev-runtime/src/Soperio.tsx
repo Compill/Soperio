389c5d43ee9770096db8c65a0ff6890771dbd408
Jonathan Gerbaud <jonathan.gerbaud@gmail.com>
2022-01-17 18:06:40 +0000

    [JSX-RUNTIME][JSX-DEV-RUNTIME] Add React.forwardRef as it wasn't working for all components

--- libs/jsx-dev-runtime/src/Soperio.tsx
+++ libs/jsx-dev-runtime/src/Soperio.tsx
@@ -1,13 +1,15 @@
 import { parseProps } from "@soperio/core";
 import { jsx as EmotionJSX } from "@emotion/react";
+import React from "react";
 
-export const SOPERIO_TYPE = "__SOPERIO_TYPE_PLEASE_DO_NOT_USE__"
+export const SOPERIO_TYPE = "__SOPERIO_TYPE_PLEASE_DO_NOT_USE__";
 
-export function Soperio(props: any)
+export const Soperio = React.forwardRef((props: any, ref) =>
 {
   const type = props[SOPERIO_TYPE];
 
   const newProps = typeof type === "string" ? parseProps(props) : props;
+  newProps.ref = ref;
 
   return EmotionJSX(type, newProps, newProps.children);
-}
+});
