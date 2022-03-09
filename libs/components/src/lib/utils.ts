3072a7cb85508129ec4d7c96eeb2e57df2a2b4e6
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2022-03-09 10:49:06 +0000

    [COMPONENTS] Fixed splitComponentProps

--- libs/components/src/lib/utils.ts
+++ libs/components/src/lib/utils.ts
@@ -1,58 +1,5 @@
+import { CSSPropKeys } from "@soperio/theming";
 import { omit, split } from "@soperio/utils";
-
-const accessibility = ["srOnly", "notSrOnly"]
-const background = ["bgAtt", "bgClip", "bgColor", "bgOpacity", "bgImage", "bgOrigin", "bgPosition", "bgRepeat", "bgSize"/*, "bgGradient", "from", "via", "to"*/]
-const border = [
-    "rounded", "roundedT", "roundedB", "roundedS", "roundedE",
-    "border", "borderT", "borderB", "borderS", "borderE",
-    "borderColor", "borderColorT", "borderColorB", "borderColorS", "borderColorE",
-    "borderOpacity", "borderStyle",
-    "divideX", "divideY",
-    "divideXReverse", "divideYReverse",
-    "divideColor", "divideOpacity", "divideStyle",
-    // "ringColor", "ringOffset", "ringOffsetColor", "ringOpacity", "ringWidth"
-]
-
-const effects = ["shadow", "opacity", "mixBlend", "bgBlend"]
-const filters = [
-    "blur", "brightness", "contrast", "dropShadow", "grayscale", "hueRotate", "invert", "saturate", "sepia",
-    "backdropBlur", "backdropBrightness", "backdropContrast", "backdropGrayscale", "backdropHueRotate", "backdropInvert", "backdropOpacity", "backdropSaturate", "backdropSepia",
-]
-const flexbox = [ "flexRow", "flexCol","flexDirection","flexWrap","flex","flexGrow","flexShrink","order","gridCols","colSpan","colStart","colEnd","gridRows","rowSpan","rowStart","rowEnd","gridFlow","gridAutoCols","gridAutoRows","gap","gapX","gapY","justifyContent","justifyItems","justifySelf","alignContent","alignItems","alignSelf","placeContent","placeItems","placeSelf" ]
-
-const interactivity = [ "appearanceNone","cursor","outline","pointerEvents","resize","userSelect"]
-
-const layout = [ "boxDecorationBreak", "boxSizing","block","dflex","display","inline","grid","hidden","float","clear","isolation","objectFit","objectPosition","overflow","overflowX","overflowY","overscrollX","overscrollY","overscrollBehavior","overscrollBehaviorX","overscrollBehaviorY","position","top","bottom","start","end","inset","insetX","insetY","visible","invisible","z" ]
-
-const sizing = [ "w", "h", "minW", "minH", "maxW", "maxH" ]
-const spacing = [ "p", "pt", "pb", "ps", "pe", "px", "py", "m", "mt", "mb", "ms", "me", "mx", "my", "spaceX", "spaceY", "spaceXReverse", "spaceYReverse" ]
-
-const svg = ["fillCurrent", "strokeCurrent", "strokeWidth" ]
-const table = ["borderCollapse", "tableLayout" ]
-const transform = [ "transform", "transformOrigin", "scale", "scaleX", "scaleY", "scaleZ", "rotate", "rotateX", "rotateY", "rotateZ", "skewX", "skewY", "translateX", "translateY", "translateZ" ]
-const transitions = [ "transition", "duration", "easing", "delay", "animate" ]
-const typography = [ "font", "fontSize", "italic", "notItalic", "fontWeight", "numericFontVariant", "letterSpacing", "lineHeight", "listStyle", "listStylePosition", "placeholderColor", "placeholderOpacity", "textAlign", "textColor", "textOpacity", "textDecoration", "textTransform", "textOverflow", "verticalAlign", "whitespace", "wordBreak", "textColumns", "textColumnsGap" ]
-
-const props = ([] as string[]).concat(accessibility)
-    .concat(background)
-    .concat(border)
-    .concat(effects)
-    .concat(filters)
-    .concat(flexbox)
-    .concat(interactivity)
-    .concat(layout)
-    .concat(sizing)
-    .concat(spacing)
-    .concat(svg)
-    .concat(table)
-    .concat(transform)
-    .concat(transitions)
-    .concat(typography)
-
-
-
-// TODO: Find a way to build Soperio props array
-const CSSPropKeys: string[] = [];
 
 /**
    * Returns all props that are not base Soperio props (like bgColor, m, hover_xx, ...)
--- libs/theming/src/lib/CSSProps.ts
+++ libs/theming/src/lib/CSSProps.ts
@@ -50,3 +50,65 @@ export type ResponsiveCSSProps =
     & LG<CSSProps>
     & XL<CSSProps>
     & XXL<CSSProps>
+
+const accessibility = ["srOnly", "notSrOnly"];
+const background = ["bgAtt", "bgClip", "bgColor", "bgOpacity", "bgImage", "bgOrigin", "bgPosition", "bgRepeat", "bgSize"/*, "bgGradient", "from", "via", "to"*/];
+const border = [
+    "rounded", "roundedT", "roundedB", "roundedS", "roundedE",
+    "border", "borderT", "borderB", "borderS", "borderE",
+    "borderColor", "borderColorT", "borderColorB", "borderColorS", "borderColorE",
+    "borderOpacity", "borderStyle",
+    "divideX", "divideY",
+    "divideXReverse", "divideYReverse",
+    "divideColor", "divideOpacity", "divideStyle",
+    // "ringColor", "ringOffset", "ringOffsetColor", "ringOpacity", "ringWidth"
+];
+
+const effects = ["shadow", "opacity", "mixBlend", "bgBlend"];
+const filters = [
+    "blur", "brightness", "contrast", "dropShadow", "grayscale", "hueRotate", "invert", "saturate", "sepia",
+    "backdropBlur", "backdropBrightness", "backdropContrast", "backdropGrayscale", "backdropHueRotate", "backdropInvert", "backdropOpacity", "backdropSaturate", "backdropSepia",
+];
+const flexbox = ["flexRow", "flexCol", "flexDirection", "flexWrap", "flex", "flexGrow", "flexShrink", "order", "gridCols", "colSpan", "colStart", "colEnd", "gridRows", "rowSpan", "rowStart", "rowEnd", "gridFlow", "gridAutoCols", "gridAutoRows", "gap", "gapX", "gapY", "justifyContent", "justifyItems", "justifySelf", "alignContent", "alignItems", "alignSelf", "placeContent", "placeItems", "placeSelf"];
+
+const interactivity = ["appearanceNone", "cursor", "outline", "pointerEvents", "resize", "userSelect"];
+
+const layout = ["boxDecorationBreak", "boxSizing", "block", "dflex", "display", "inline", "grid", "hidden", "float", "clear", "isolation", "objectFit", "objectPosition", "overflow", "overflowX", "overflowY", "overscrollX", "overscrollY", "overscrollBehavior", "overscrollBehaviorX", "overscrollBehaviorY", "position", "top", "bottom", "start", "end", "inset", "insetX", "insetY", "visible", "invisible", "z"];
+
+const sizing = ["w", "h", "minW", "minH", "maxW", "maxH"];
+const spacing = ["p", "pt", "pb", "ps", "pe", "px", "py", "m", "mt", "mb", "ms", "me", "mx", "my", "spaceX", "spaceY", "spaceXReverse", "spaceYReverse"];
+
+const svg = ["fillCurrent", "strokeCurrent", "strokeWidth"];
+const table = ["borderCollapse", "tableLayout"];
+const transform = ["transform", "transformOrigin", "scale", "scaleX", "scaleY", "scaleZ", "rotate", "rotateX", "rotateY", "rotateZ", "skewX", "skewY", "translateX", "translateY", "translateZ"];
+const transitions = ["transition", "duration", "easing", "delay", "animate"];
+const typography = ["font", "fontSize", "italic", "notItalic", "fontWeight", "numericFontVariant", "letterSpacing", "lineHeight", "listStyle", "listStylePosition", "placeholderColor", "placeholderOpacity", "textAlign", "textColor", "textOpacity", "textDecoration", "textTransform", "textOverflow", "verticalAlign", "whitespace", "wordBreak", "textColumns", "textColumnsGap"];
+
+const props = ([] as string[]).concat(accessibility)
+    .concat(background)
+    .concat(border)
+    .concat(effects)
+    .concat(filters)
+    .concat(flexbox)
+    .concat(interactivity)
+    .concat(layout)
+    .concat(sizing)
+    .concat(spacing)
+    .concat(svg)
+    .concat(table)
+    .concat(transform)
+    .concat(transitions)
+    .concat(typography);
+
+function buildBreakpointProps(breakpoint:string, props:string[]): string[]
+{
+    const prefix = `${breakpoint}_`
+    return props.map((value, index) => `${prefix}${value}`)
+}
+
+export const CSSPropKeys = ([] as string[])
+    .concat(buildBreakpointProps("sm", props))
+    .concat(buildBreakpointProps("md", props))
+    .concat(buildBreakpointProps("lg", props))
+    .concat(buildBreakpointProps("xl", props))
+    .concat(buildBreakpointProps("xxl", props))
