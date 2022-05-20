b28991e9c1baedbdd4c24b9156248336074c1a35
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2022-05-20 14:59:56 +0300

    Fix deploy script

--- deploy.js
+++ deploy.js
@@ -26,13 +26,13 @@ function deploy(lib)
   console.log("Deployed!");
 
   console.log("Committing...");
-  execSync(`git-commit -m [${lib.toUpperCase()}] Version ${version}`)
+  execSync(`git-commit -a -m "[${lib.toUpperCase()}] Version ${version}"`)
 
   console.log("Pushing...");
   execSync(`git push`)
 
   console.log("Creating git tag...");
-  execSync(`git tag -a ${lib}-${version} -m \"${lib.toUpperCase()} Version ${version}`)
+  execSync(`git tag -a ${lib}-${version} -m \"${lib.toUpperCase()} Version ${version}"`)
 
   console.log(`[END] Finished deploying ${lib} ${version}`)
 }
