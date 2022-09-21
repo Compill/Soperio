c740d3cd646ee54d2a10c9303ed0844f508b79a8
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2022-09-21 17:00:42 +0100

    [CLI] Version 1.0.52

--- libs/cli/src/index.ts
+++ libs/cli/src/index.ts
@@ -46,7 +46,7 @@ export async function run()
       const { out } = options;
 
       const themeInterface = ["node_modules", "@soperio", "theming", "lib", "ThemeTypings.d.ts"];
-      await overwriteFrameworkFile("Generating Soperio theme typings", themeFile, out, themeInterface, ["theme", "worker.js"]);
+      await overwriteFrameworkFile("Generating Soperio theme typings", themeFile, out, themeInterface, ["theme", "worker.ts"]);
     });
 
     // TODO Commands to generate either theme or components typings
