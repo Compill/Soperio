b1dd83f70fbb7257d1ab810870166ada2a2cee11
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2022-09-26 23:03:45 +0100

    [THEMING] Fix root color parsing with useColor

--- libs/theming/src/lib/hooks/useColor.ts
+++ libs/theming/src/lib/hooks/useColor.ts
@@ -12,10 +12,10 @@ export function useColor(color: Color): string
   // We don't need to check for darkMode since root colors are not managed the same way as colors
   // But are handled as CSS vars. Thus, whether dark mode or not, they are only referenced by name
   // and we are not looking to parse their values
-  if (color.startsWith("--") && theme.rootColors[color.substring(5)])
-    return `rgb(var(--so-${color.substring(5)}))`
+  if (color.startsWith("--") && theme.rootColors[color.substring(2)])
+    return `rgb(var(--so-${color.substring(2)}))`
   else if (color.startsWith("--") && IS_DEV)
-    console.warn(`[SOPERIO] You seem to want to use root color "${color.substring(5)}" but it doesn't exist in your theme`)
+    console.warn(`[SOPERIO] You seem to want to use root color "${color.substring(2)}" but it doesn't exist in your theme`)
 
   // Can be transparent, blue, #FF0000, rgb(x, x, x), ...
   return color;
