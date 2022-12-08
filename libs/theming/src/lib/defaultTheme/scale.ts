2e9d7bf040b76e4f6f630679e023d59f03db3670
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2022-12-08 16:07:41 +0700

    [THEMING] Added larger scale values

--- libs/theming/src/lib/defaultTheme/scale.ts
+++ libs/theming/src/lib/defaultTheme/scale.ts
@@ -48,6 +48,13 @@ export const scale = {
   "72": "18rem",
   "80": "20rem",
   "96": "24rem",
+  "128": "32rem",
+  "144": "36rem",
+  "160": "40rem",
+  "192": "48rem",
+  "224": "56rem",
+  "256": "64rem",
+  "288": "72rem",
 }
 
 export const negativeScale = negative(scale)
