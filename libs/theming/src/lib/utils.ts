29cdb55ced44bc587e37e69161ebd165bbbc7159
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2023-05-28 21:10:36 +0700

    [THEMING] Fix splitCompnentProps typing

--- libs/theming/src/lib/utils.ts
+++ libs/theming/src/lib/utils.ts
@@ -20,7 +20,7 @@ export function omitComponentProps(props: Record<string, any>): Record<string, a
  * @param props
  * @returns
  */
-export function splitComponentProps(props: Record<string, any>): [SoperioComponent, Record<string, any>[]]
+export function splitComponentProps(props: Record<string, any>): [SoperioComponent, Record<string, any>]
 {
-    return split(props, CSSPropKeys) as [SoperioComponent, Record<string, any>[]];
+    return split(props, CSSPropKeys)// as [SoperioComponent, Record<string, any>[]];
 };
