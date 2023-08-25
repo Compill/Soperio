98decf7c87fc56dda361ee387986901fc404eb69
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2023-08-25 15:37:46 +0200

    [THEMING] Adding breathe animation

--- libs/theming/src/lib/Core/Transition.ts
+++ libs/theming/src/lib/Core/Transition.ts
@@ -3,6 +3,6 @@ export default interface ThemeTransition
   ease: "linear" | "in" | "out" | "in-out",
   duration: "75" | "100" | "150" | "200" | "300" | "500" | "700" | "1000",
   delay: "75" | "100" | "150" | "200" | "300" | "500" | "700" | "1000",
-  animation: "none" | "spin" | "ping" | "pulse",
+  animation: "none" | "spin" | "ping" | "pulse" | "bounce" | "breathe",
   transitionProperty: "all" | "none" | "colors" | "opacity" | "shadow" | "transform";
 }
--- libs/theming/src/lib/defaultTheme/transition.ts
+++ libs/theming/src/lib/defaultTheme/transition.ts
@@ -40,40 +40,49 @@ const transition = {
     ping: "1s cubic-bezier(0, 0, 0.2, 1) infinite",
     pulse: "2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
     bounce: "1s infinite",
+    breathe: "breathing ease-in-out infinite alternate"
   },
   keyframes: {
     spin: `
-        from {
-          transform: rotate(0deg);
-        }
-        to {
-          transform: rotate(360deg);
-        }
-      `,
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    `,
     ping: `
-        75%, 100% {
-          transform: scale(2);
-          opacity: 0;
-        }
-      `,
+      75%, 100% {
+        transform: scale(2);
+        opacity: 0;
+      }
+    `,
     pulse: `
-        0%, 100% {
-          opacity: 1;
-        }
-        50% {
-          opacity: .5;
-        }
-      `,
+      0%, 100% {
+        opacity: 1;
+      }
+      50% {
+        opacity: .5;
+      }
+    `,
     bounce: `
-        0%, 100% {
-          transform: translateY(-25%);
-          animation-timing-function: cubic-bezier(0.8, 0, 1, 1);
-        }
-        50% {
-          transform: translateY(0);
-          animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
-        }
-      `,
+      0%, 100% {
+        transform: translateY(-25%);
+        animation-timing-function: cubic-bezier(0.8, 0, 1, 1);
+      }
+      50% {
+        transform: translateY(0);
+        animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+      }
+    `,
+    breathe: `
+      from {
+        opacity: 0.25;
+      }
+      to {
+        opacity: 1;
+      }
+    `
   },
 }
 
