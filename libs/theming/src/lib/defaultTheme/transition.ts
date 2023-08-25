ca5f5983eb64cc9ee5a3e7767b4fd235bcd871fc
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2023-08-25 17:22:22 +0200

    [THEMING] Forgot animation duration

--- libs/theming/src/lib/defaultTheme/transition.ts
+++ libs/theming/src/lib/defaultTheme/transition.ts
@@ -40,7 +40,7 @@ const transition = {
     ping: "1s cubic-bezier(0, 0, 0.2, 1) infinite",
     pulse: "2s cubic-bezier(0.4, 0, 0.6, 1) infinite",
     bounce: "1s infinite",
-    breathe: "ease-in-out infinite alternate"
+    breathe: "1s ease-in-out infinite alternate"
   },
   keyframes: {
     spin: `
