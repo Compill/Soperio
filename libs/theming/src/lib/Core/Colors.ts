7ef49aa9a905aa82cc4af67f25a683a4f270669c
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2023-10-16 16:32:03 +0100

    [THEMING] Remove black-alpha and white-alpha typings from colors

--- libs/theming/src/lib/Core/Colors.ts
+++ libs/theming/src/lib/Core/Colors.ts
@@ -1,49 +1,5 @@
 type ThemeColors =
   "transparent" |
-  "current" |
-
-  "black-alpha-100" |
-  "black-alpha-95" |
-  "black-alpha-90" |
-  "black-alpha-85" |
-  "black-alpha-80" |
-  "black-alpha-75" |
-  "black-alpha-70" |
-  "black-alpha-65" |
-  "black-alpha-60" |
-  "black-alpha-55" |
-  "black-alpha-50" |
-  "black-alpha-45" |
-  "black-alpha-40" |
-  "black-alpha-35" |
-  "black-alpha-30" |
-  "black-alpha-25" |
-  "black-alpha-20" |
-  "black-alpha-15" |
-  "black-alpha-10" |
-  "black-alpha-05" |
-  "black-alpha-0" |
-
-  "white-alpha-100" |
-  "white-alpha-95" |
-  "white-alpha-90" |
-  "white-alpha-85" |
-  "white-alpha-80" |
-  "white-alpha-75" |
-  "white-alpha-70" |
-  "white-alpha-65" |
-  "white-alpha-60" |
-  "white-alpha-55" |
-  "white-alpha-50" |
-  "white-alpha-45" |
-  "white-alpha-40" |
-  "white-alpha-35" |
-  "white-alpha-30" |
-  "white-alpha-25" |
-  "white-alpha-20" |
-  "white-alpha-15" |
-  "white-alpha-10" |
-  "white-alpha-05" |
-  "white-alpha-0"
+  "current" | "black" | "white"
 
   export default ThemeColors
