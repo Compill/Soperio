bda73455a37d220cbd7542cf264a1f459fef7bbb
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2024-04-25 13:25:24 +0700

    [CSS] Fix boderX & Y when value is false or undefined

--- libs/core/src/lib/CSS/Border.ts
+++ libs/core/src/lib/CSS/Border.ts
@@ -19,6 +19,9 @@ function divideX(value: any, theme: Theme, direction: boolean, darkMode: boolean
 {
   const dimension = getBorderValue(value, theme)
 
+  if (value != 0 && !value)
+    return {}
+
   return {
     "--so-divide-x-reverse": 0,
     "> * + *":
@@ -32,6 +35,9 @@ function divideX(value: any, theme: Theme, direction: boolean, darkMode: boolean
 function divideY(value: any, theme: Theme, direction: boolean, darkMode: boolean): Style
 {
   const dimension = value === true ? "1px" : getBorderValue(value, theme)
+
+  if (value != 0 && !value)
+    return {}
 
   return {
     "--so-divide-y-reverse": 0,
