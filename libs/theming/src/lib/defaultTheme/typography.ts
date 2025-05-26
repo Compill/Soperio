c7cb45f94b76b178beb102c7c314b10b885c1536
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2025-05-26 11:34:34 +0700

    [THEMING] Fix theme fonts that were associated to nothing

--- libs/theming/src/lib/defaultTheme/typography.ts
+++ libs/theming/src/lib/defaultTheme/typography.ts
@@ -1,8 +1,8 @@
 const typography = {
   font: {
-    sans: "sans",
-    serif: "serif",
-    mono: "mono"
+    sans: "ui-sans-serif, system-ui, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji",
+    serif: "ui-serif, Georgia, Cambria, 'Times New Roman', Times, serif",
+    mono: "ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace"
   },
   textSize: {
     xs: "0.75rem",
