4ff7a419ca1d3e42b3d00a6acdb5e1d426390637
Jonathan Gerbaud <jonathan.gerbaud@abewy.com>
2025-10-26 15:37:12 +0700

    [JSX-DEV-RUNTIME-SEREVER] I don't get it...

--- libs/jsx-dev-runtime-server/src/jsx-dev-runtime.ts
+++ libs/jsx-dev-runtime-server/src/jsx-dev-runtime.ts
@@ -1,22 +1,22 @@
 import { ElementType } from "react";
 import * as ReactJSXRuntimeDev from 'react/jsx-dev-runtime';
 import { SoperioJSX } from "./jsx-namespace";
-import { parseProps } from "./parseProps";
+// import { parseProps } from "./parseProps";
 
 
 export { Fragment } from 'react';
 export type { SoperioJSX as JSX } from './jsx-namespace';
 
-const typePropName = '__SOPERIO_TYPE_PLEASE_DO_NOT_USE__'
+// const typePropName = '__SOPERIO_TYPE_PLEASE_DO_NOT_USE__'
 
-function createSoperioProps(type: ElementType, props: any)
-{
-  const asType = props["as"]
-  delete props["as"]
-  return { ...props, [typePropName]: asType ?? type };
-}
+// function createSoperioProps(type: ElementType, props: any)
+// {
+//   const asType = props["as"]
+//   delete props["as"]
+//   return { ...props, [typePropName]: asType ?? type };
+// }
 
-const nonStyleableHtmlTags = ["html", "head", "link", "meta", "script", "title", "body", "style", "base"]
+// const nonStyleableHtmlTags = ["html", "head", "link", "meta", "script", "title", "body", "style", "base"]
 
 export function jsxDEV<P>(
   type: ElementType<P>,
@@ -32,14 +32,14 @@ export function jsxDEV<P>(
 ): SoperioJSX.Element
 {
   console.log("JSX-DEV-RUNTIME-SERVER", type, props)
-  // @ts-ignore
-  const isServer = props.server
+  // // @ts-ignore
+  // const isServer = props.server
 
-  if (!isServer && /*isClient &&*/ typeof type === "string" && !nonStyleableHtmlTags.includes(type))
-  {
-    // @ts-ignore
-    return ReactJSXRuntimeDev.jsxDEV(type, parseProps(createSoperioProps(type, props)), key, isStaticChildren, source, self);
-  }
+  // if (!isServer && /*isClient &&*/ typeof type === "string" && !nonStyleableHtmlTags.includes(type))
+  // {
+  //   // @ts-ignore
+  //   return ReactJSXRuntimeDev.jsxDEV(type, parseProps(createSoperioProps(type, props)), key, isStaticChildren, source, self);
+  // }
 
   const _props = { ...props }
   delete _props["server"]
